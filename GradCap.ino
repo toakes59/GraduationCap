ae64c05bbed5112234ce33df6e1cb32cd25179c3
toakes59 <31904863+toakes59@users.noreply.github.com>
2019-04-17 11:59:10 -0400

    This is the first upload
    The GradCap.ino is going to be the main code, and the Gradsong.ino is my attempt at the graduation song.  The Gradsong will be implemented into GradCap later

---
+++ GradCap.ino
@@ -0,0 +1,41 @@
+#include <Servo.h>
+
+const int buttonPin = 4;
+const int servoPin = 7;
+const int buzzerPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(servoPin);
+
+  pinMode(buttonPin, INPUT)
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  
+  if(buttonState == HIGH){
+
+    
+    
+    /*servo.write(20);
+    servo.write(30);
+    servo.write(40);
+    servo.write(50);
+    servo.write(60);
+    servo.write(70);
+    servo.write(80);
+    servo.write(90);
+    servo.write(100);
+    servo.write(110);
+    servo.write(120);
+    servo.write(130);
+    servo.write(140);
+    servo.write(150);
+    servo.write(160);
+    servo.write(170);
+    servo.write(180);//Ending Position*/
+    
+  }
+}
