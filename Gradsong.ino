85595ab73f15dad89429e8a7b1652acc1f0806f2
toakes59 <31904863+toakes59@users.noreply.github.com>
2019-04-17 12:24:42 -0400

    Added different notes
    Changed the notes so that I could hit certain notes in the song.  Before was unable to hit higher notes so I brought it down a bit.

--- Gradsong.ino
+++ Gradsong.ino
@@ -1,7 +1,7 @@
 // TONES  ==========================================
 // Start by defining the relationship between 
 //       note, period, &  frequency. 
-#define  c     3830    // 261 Hz 
+/*#define  c     3830    // 261 Hz 
 #define  d     3400    // 294 Hz 
 #define  e     3038    // 329 Hz 
 #define  f     2864    // 349 Hz 
@@ -9,8 +9,98 @@
 #define  a     2272    // 440 Hz 
 #define  b     2028    // 493 Hz 
 #define  C     1912    // 523 Hz 
-// Define a special note, 'R', to represent a rest
-#define  R     0
+#define  R     0*/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+//Here Down for notes
+#define C6  1047
+#define CS6 1109
+#define D6  1175
+#define DS6 1245
+#define E6  1319
+#define F6  1397
+#define FS6 1480
+#define G6  1568
+#define GS6 1661
+#define A6  1760
+#define AS6 1865
+#define B6  1976
+#define C7  2093
+#define CS7 2217
+#define D7  2349
+#define DS7 2489
+#define E7  2637
+#define F7  2794
+#define FS7 2960
+#define G7  3136
+#define GS7 3322
+#define A7  3520
+#define AS7 3729
+#define B7  3951
+#define C8  4186
+#define CS8 4435
+#define D8  4699
+#define DS8 4978
 
 // SETUP ============================================
 // Set up speaker on a PWM pin (digital 9, 10 or 11)
@@ -28,8 +118,11 @@ void setup() {
 // MELODY and TIMING  =======================================
 //  melody[] is an array of notes, accompanied by beats[], 
 //  which sets each note's relative length (higher #, longer note) 
-int melody[] = { C,  C,  b,  C,  d,   a,  g,  f,  f,  e, f, g,  d,  e,  f,  g, a,  d,  g};
-int beats[]  = { 64, 64, 32, 32, 64, 128, 128, 64, 64, 32, 32, 64, 256, 128, 32, 64, 32, 128, 128 }; 
+//Old Notes
+//int melody[] = { C,  C,  b,  C,  d,   a,  g,  f,  f,  e, f, g,  d,  e,  f,  g, a,  d,  g};
+//New Notes
+int melody[] = { C7, C7, B6, C7, D6,  A6,  G6, F6, F6, E6, F6, G6,  D6,  E6, F6, G6, A6,  D7, G6};
+int beats[]  = { 64, 64, 32, 32, 64, 128, 128, 64, 64, 32, 32, 64, 256, 128, 32, 64, 32, 128, 128}; 
 int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
 
 // Set overall tempo
@@ -94,4 +187,3 @@ void loop() {
     }
   }
 }
-
